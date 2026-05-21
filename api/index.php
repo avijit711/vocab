7924e3a79780be7ba5ea5e785e690a4ea91a9da1
Avijit Biwas <avijitbiwas@Avijits-MacBook-Air.local>
2026-05-21 21:24:36 +0600

    Simplify Vercel config: forward via public/index.php, exclude storage from deploy

--- api/index.php
+++ api/index.php
@@ -1,14 +1,5 @@
 <?php
 
-use Illuminate\Foundation\Application;
-use Illuminate\Http\Request;
-
-define('LARAVEL_START', microtime(true));
-
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
-    require $maintenance;
-}
-
 $dbPath = getenv('DB_DATABASE') ?: '';
 if ($dbPath && !file_exists($dbPath) && str_starts_with($dbPath, '/tmp/')) {
     $dir = dirname($dbPath);
@@ -18,14 +9,4 @@ if ($dbPath && !file_exists($dbPath) && str_starts_with($dbPath, '/tmp/')) {
     touch($dbPath);
 }
 
-require __DIR__.'/../vendor/autoload.php';
-
-$app = require_once __DIR__.'/../bootstrap/app.php';
-
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-
-$response = $kernel->handle(
-    $request = Request::capture()
-)->send();
-
-$kernel->terminate($request, $response);
+require __DIR__ . '/../public/index.php';
